157a608bd85c9faf26226e2a1d1183faa16b554f
Samandar000 <samandarnfh.2020@gmail.com>
2023-02-14 20:28:27 +0500

    python bot

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,request
 import os
 import requests
-
+from telegram import Bot, Update
 
 
 
@@ -9,13 +9,22 @@ import requests
 app = Flask(__name__)
 
 TOKEN = os.environ['TOKEN']
+bot = Bot(TOKEN)
 
-@app.route('/webhook',methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
-     
-    data = requests.get_json(force=True)
+    # get data from request
+    data = request.get_json(force=True)
 
-    print(data)
+    # update
+    update: Update = Update.de_json(data, bot)
+
+    # get chat_id, text from update
+    chat_id = update.message.chat.id
+    text = update.message.text
+
+    # sendMessage
+    bot.send_message(chat_id, text)
 
     return 'ok'
 
